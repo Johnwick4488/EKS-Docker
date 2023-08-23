9eabd0c01e6fe2f141b15cf9e17eb5c922edc9a9
Santosh44 <disizsantosh@gmail.com>
2023-08-23 22:45:28 +0530

    added files

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,6 +1,6 @@
 # EKS Cluster
 resource "aws_eks_cluster" "this" {
-  name     = "${var.project}-cluster"
+  name     = "${var.eksproject}-cluster"
   role_arn = aws_iam_role.cluster.arn
   version  = "1.25"
 
@@ -24,7 +24,7 @@ resource "aws_eks_cluster" "this" {
 
 # EKS Cluster IAM Role
 resource "aws_iam_role" "cluster" {
-  name = "${var.project}-Cluster-Role"
+  name = "${var.eksproject}-Cluster-Role"
 
   assume_role_policy = <<POLICY
 {
@@ -50,31 +50,31 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
 
 # EKS Cluster Security Group
 resource "aws_security_group" "eks_cluster" {
-  name        = "${var.project}-cluster-sg"
+  name        = "${var.eksproject}-cluster-sg"
   description = "Cluster communication with worker nodes"
   vpc_id      = aws_vpc.this.id
 
   tags = {
-    Name = "${var.project}-cluster-sg"
+    Name = "${var.eksproject}-cluster-sg"
   }
 }
 
-resource "aws_security_group_rule" "cluster_inbound" {
-  description              = "Allow worker nodes to communicate with the cluster API Server"
-  from_port                = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks_cluster.id
-  source_security_group_id = aws_security_group.eks_nodes.id
-  to_port                  = 443
-  type                     = "ingress"
-}
+# resource "aws_security_group_rule" "cluster_inbound" {
+#   description              = "Allow worker nodes to communicate with the cluster API Server"
+#   from_port                = 443
+#   protocol                 = "tcp"
+#   security_group_id        = aws_security_group.eks_cluster.id
+#   source_security_group_id = aws_security_group.eks_nodes.id
+#   to_port                  = 443
+#   type                     = "ingress"
+# }
 
-resource "aws_security_group_rule" "cluster_outbound" {
-  description              = "Allow cluster API Server to communicate with the worker nodes"
-  from_port                = 1024
-  protocol                 = "tcp"
-  security_group_id        = aws_security_group.eks_cluster.id
-  source_security_group_id = aws_security_group.eks_nodes.id
-  to_port                  = 65535
-  type                     = "egress"
-}
+# resource "aws_security_group_rule" "cluster_outbound" {
+#   description              = "Allow cluster API Server to communicate with the worker nodes"
+#   from_port                = 1024
+#   protocol                 = "tcp"
+#   security_group_id        = aws_security_group.eks_cluster.id
+#   source_security_group_id = aws_security_group.eks_nodes.id
+#   to_port                  = 65535
+#   type                     = "egress"
+# }
